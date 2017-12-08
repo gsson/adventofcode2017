765a72aeb7d272c47096aa4ba56c170e79a7950e
Henrik Gustafsson <henrik.gustafsson@ticketmaster.co.uk>
2017-12-08 12:53:53 +0000

    Day 5 with tail recursion

--- src/test/scala/Day5.scala
+++ src/test/scala/Day5.scala
@@ -1,8 +1,25 @@
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.annotation.tailrec
 import scala.io.Source
 
 class Day5A extends FlatSpec with Matchers {
+  def solverRec(input: Int*): Int = {
+    @tailrec
+    def next(pc: Int, n: Int, data: IndexedSeq[Int]): Int = {
+      if (pc < 0 || pc >= data.length) {
+        n
+      }
+      else {
+        val jumpOffset = data(pc)
+        next(pc + jumpOffset, n + 1, data.updated(pc, jumpOffset + 1))
+      }
+    }
+
+    next(0, 0, input.toIndexedSeq)
+  }
+
+
   def solver(input: Int*): Int = {
     val data = Array(input: _*)
 
@@ -21,16 +38,49 @@ class Day5A extends FlatSpec with Matchers {
   "Solver" should "print result" in {
     val input = Source.fromResource("Day5.txt").getLines()
       .map(_.toInt)
-      .toSeq
-    println(solver(input: _*))
+      .toIndexedSeq
+    println(solver(input :_*))
   }
 
   it should "pass test vectors" in {
     solver(0, 3, 0, 1, -3) should be(5)
   }
+
+  "Recursive solver" should "print result" in {
+    val input = Source.fromResource("Day5.txt").getLines()
+      .map(_.toInt)
+      .toIndexedSeq
+    println(solverRec(input :_*))
+  }
+
+  it should "pass test vectors" in {
+    solverRec(0, 3, 0, 1, -3) should be(5)
+  }
 }
 
 class Day5B extends FlatSpec with Matchers {
+  def solverRec(input: Int*): Int = {
+    def nextJumpOffset(jumpOffset: Int) = {
+      if (jumpOffset >= 3)
+        jumpOffset - 1
+      else
+        jumpOffset + 1
+    }
+
+    @tailrec
+    def next(pc: Int, n: Int, data: IndexedSeq[Int]): Int = {
+      if (pc < 0 || pc >= data.length) {
+        n
+      }
+      else {
+        val jumpOffset = data(pc)
+        next(pc + jumpOffset, n + 1, data.updated(pc, nextJumpOffset(jumpOffset)))
+      }
+    }
+
+    next(0, 0, input.toIndexedSeq)
+  }
+
   def solver(input: Int*): Int = {
     val data = Array(input: _*)
 
@@ -59,5 +109,6 @@ class Day5B extends FlatSpec with Matchers {
   it should "pass test vectors" in {
 
     solver(0, 3, 0, 1, -3) should be(10)
+    solverRec(0, 3, 0, 1, -3) should be(10)
   }
 }
